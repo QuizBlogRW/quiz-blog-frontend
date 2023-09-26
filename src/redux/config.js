103152c9ada59d4111ee2fee626cd008e47e6add
Nide17 <nidedrogba@gmail.com>
2023-09-26 02:57:48 +0200

    - and 60 secs

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,2 +1,2 @@
 export const apiURL = 'https://quiz-blog-rw-server.onrender.com/'
-export const devApiURL = 'https://quiz-blog-rw-server.onrender.com/'
+export const devApiURL = 'http://localhost:4000/'
