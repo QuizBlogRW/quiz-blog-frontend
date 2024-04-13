33ff0ba1e928e579d94113b2c6002ff1fa13cc1e
Nide17 <parmenin@andrew.cmu.edu>
2024-04-13 12:22:56 +0200

    test backend

--- src/redux/configHelpers.jsx
+++ src/redux/configHelpers.jsx
@@ -3,13 +3,14 @@ import { returnErrors } from './slices/errorSlice'
 import { returnSuccess } from './slices/successSlice'
 
 export const qbURL = 'https://myqb-245fdbd30c9b.herokuapp.com/'
+export const qbTestURL = 'https://qb-test-c6396eeaa356.herokuapp.com/'
 export const apiURL = 'https://quiz-blog-rw-server.onrender.com/'
 export const devApiURL = 'http://localhost:4000/'
 
 // Axios instance
 const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? devApiURL : (qbURL || apiURL),
-    // baseURL: apiURL,
+    // baseURL: process.env.NODE_ENV === 'development' ? devApiURL : (qbURL || apiURL),
+    baseURL: qbTestURL,
 })
 
 // Function to format action type
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,11 @@
 import { io } from 'socket.io-client'
 
 export const qbURL = 'https://myqb-245fdbd30c9b.herokuapp.com/'
+export const qbTestURL = 'https://qb-test-c6396eeaa356.herokuapp.com/'
 export const apiURL = 'https://quiz-blog-rw-server.onrender.com/'
 export const devApiURL = 'http://localhost:4000/'
 
-const serverUrl = process.env.NODE_ENV === 'development' ? devApiURL : (qbURL || apiURL)
-// const serverUrl = apiURL
+// const serverUrl = process.env.NODE_ENV === 'development' ? devApiURL : (qbURL || apiURL)
+const serverUrl = qbTestURL
 
 export const socket = io(serverUrl);
